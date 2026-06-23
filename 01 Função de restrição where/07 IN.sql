a9da45a0cd2d56bb5f9ee624730b3fff650594b0
Jailson Carvalho <147885532+Jailson-Carvalho@users.noreply.github.com>
2026-06-23 16:31:54 -0300

    Update and rename 07 IN.sql to 07 IN.md

--- 01 Função de restrição where/07 IN.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-
-FROM DimEmployee
-
---WHERE EmployeeKey IN (2,5,7)
-WHERE FirstName NOT IN ('Kevin', 'Rob', 'Jossef')
-AND DepartmentName IN ('Marketing', 'Production')
-
-ORDER BY DepartmentName
